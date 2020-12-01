51dc2cdea9d6d3ad3f2c2bab1186e25642a53164
GregorySkuza <gskuza94@gmail.com>
2020-12-01 14:04:30 +0100

    Initial commit

---
+++ src/main/java/pl/gskuza/accountauthentication/AccountAuthenticationApplication.java
@@ -0,0 +1,13 @@
+package pl.gskuza.accountauthentication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountAuthenticationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AccountAuthenticationApplication.class, args);
+    }
+
+}
